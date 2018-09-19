14b70c6d9bb9a59d177d5501e76b36f16cb9c0dd
Chris <Shimohara4@gmail.com>
2018-09-19 09:52:19 -0400

    For Adrian

--- SS_2D.m
+++ SS_2D.m
@@ -1,61 +1,39 @@
 clear all;
-rng('default');
+rng(1);
 
 % adjust time step 
 delt = 0.001; % time step length in secs
 nstep = ceil(22/delt); % number of time steps
 start = [0 0]; % starting position of the hand
 
-% parameters for A and B matrices
-t1 = 0.224;
-t2 = 0.013;
-t3 = 0.004;
-k = 0; %1 to include spring
-b = t1 + t2;
-m = t1*t2;
-r = t3;
+% Single joint reaching movements:
+G = .14;        % Viscous Constant: Ns/m
+I = .1;         % Inertia Kgm2
+tau = 0.066;    % Muscle time constant, s
 
 % sum-of-sines target movement
-freq = (0.05:0.05:1)'; % frequencies used in the simulation
+freq = (0.05:0.05:10)'; % frequencies used in the simulation
 phase = 2*pi*rand(length(freq),2)-pi; % phases of sum of sines
 target(1,:,:) = sin(freq*2*pi*(0:delt:nstep*delt-delt) + repmat(phase(:,1),1,nstep));
 target(2,:,:) = sin(freq*2*pi*(0:delt:nstep*delt-delt) + repmat(phase(:,2),1,nstep));
 target = squeeze(sum(target,2));
 
-% generate A, B, and C matrices in discrete time formulation
-A = [0 1 0
-    -k/m -b/m 1/m
-    0 0 -1/r];
-B = [0 0 1/r]';
-C = [1 0 0];
-H = tf(ss(A,B,C,0));
-delay = [0 0.5];
+delay = [0 0.1 0.3 0.5 0.7 1];
 ang = [0 0];
-name = {'0 s',[num2str(delay(2)),' s']};
+for j = 1:length(delay)
+    name{j} = [num2str(delay(j)),' s'];
+end
 
 for j = 1:length(delay)
-    % perform pade approximation
-    s = tf('s');
-    G = exp(-delay(j)*s);
-    G = pade(G,1);
-    J = H*G;
-    [num,den] = tfdata(J);
-    m = length(num{1});
-
     if delay(j) == 0
-        A = zeros(m-1);
-        for i = 2:m-1
-            A(i-1,i) = 1;
-            A(end,i) = -den{1}(end-i+1);
-        end
-        B = [0 0 num{1}(end)]';
+        A = [0 1 0;0 -G/I 1/I;0 0 -1/tau];
+        B = [0 0 1/tau]';
     else
-        A = [-den{1}(2) 1 0 0
-            -den{1}(3) 0 1 0
-            -den{1}(4) 0 0 1
-            0 0 0 0];
-        B = [0 0 num{1}(4) num{1}(5)]';
-        C = [1 0 0 0];
+        A = [0 1 0 0
+            0 -G/I 2/(delay(j)*I) -1/I
+            0 0 -1/delay(j) 1
+            0 0 0 1/tau];
+        B = [0 0 0 1/tau]';
     end
 
     z = size(A,1)+1;
@@ -69,11 +47,6 @@ for j = 1:length(delay)
     Bd = [Bd zeros(z,1)
          zeros(z,1) Bd];
 
-    C = rotz(ang(j));
-    C = C(1:2,1:2);
-    C = [diag(repelem(C(1,1),z)) diag(repelem(C(1,2),z))
-        diag(repelem(C(2,1),z)) diag(repelem(C(2,2),z))];
-    
     % values for Q and R taken from Qian infinite horizon model
     R = [0.1 0
         0 0.1]; % effort cost- default is 0.0001
@@ -96,7 +69,7 @@ for j = 1:length(delay)
     u = zeros(size(Bd,2),nstep); % movement commands
 
     % calculate feedback gain
-    n = 100000;
+    n = 10000;
     P = zeros(order,order,n);
     P(:,:,1) = rand(order);
     for i = 2:n
@@ -106,9 +79,8 @@ for j = 1:length(delay)
 
     % simulate trajectory
     for i = 2:nstep
-        u(:,i) = -L*inv(C)*x(:,i-1);
+        u(:,i) = -L*x(:,i-1);
         x(:,i) = Ad*x(:,i-1) + Bd*u(:,i);
-    %     xhat(:,i) = C*xhat(:,i-1) + Bd*u(:,i-1);
 
         % set target location
         x(end/2,i) = target(1,i);
@@ -117,23 +89,23 @@ for j = 1:length(delay)
         xhat(end,i) = target(2,i);
     end
 
-    figure(1)
-    subplot(1,2,j)
-    plot(x(1,:),x(end/2+1,:))
-    hold on
-    title(['Delay = ',num2str(delay(j)),' s']);
-    plot(x(end/2,2:end),x(end,2:end))
-    legend({'Hand','Target'})
-    pbaspect([1 1 1]);
+%     figure(1)
+%     subplot(1,2,j)
+%     plot(x(1,:),x(end/2+1,:))
+%     hold on
+%     title(['Delay = ',num2str(delay(j)),' s']);
+%     plot(x(end/2,2:end),x(end,2:end))
+%     legend({'Hand','Target'})
+%     pbaspect([1 1 1]);
 
     % compute fourier transforms
     e = 2/delt; % figure out the number of time steps to throw away
-
-    hand = x([1 end/2+1],(e+1):end)'; % time shift hand signal by 'lag'
+    
+    hand = x([1 end/2+1],(e+1):end)';
     targ = x([end/2 end],(e+1):end)';
     hand_avg = mean(hand,1);
     targ_avg = mean(targ,1);
-
+    
     fft_in = fft(targ - repmat(targ_avg,[nstep-e 1]));
     fft_out = fft(hand - repmat(hand_avg,[nstep-e 1]));
     [inSort,i] = sort(fft_in(:,1),'descend');
@@ -145,19 +117,29 @@ for j = 1:length(delay)
     phase = unwrap(angle(ratio)); % phase
     
     figure(2)
-    subplot(2,1,1)
-    semilogx(freq, amp(:,1))
-    hold on
-    ylabel('Gain (cm/cm)')
-    ylim([0.2 1.5]);
+%     subplot(2,1,2)
+%     semilogx(freq, amp(:,1))
+%     hold on
+%     ylabel('Gain (cm/cm)')
+%     ylim([0.2 1.5]);
 %     title(['Delay = ',num2str(delay(j)),' s']);
     
-    subplot(2,1,2)
+    subplot(2,1,1)
     semilogx(freq,phase(:,1)*180/pi)
     hold on
     ylabel('Phase (degrees)')
     xlabel('Frequency (Hz)')
 %     ylim([-200 0]);
+    time(:,j) = phase(:,1)*180/pi;
+    time(:,j) = time(:,j) - time(:,1);
 end
-figure(2)
+
+time = -1000*time./360;
+subplot(2,1,2)
+semilogx(freq,time)
+xlabel('Frequency (Hz)')
+ylabel('Time Difference from Undelayed (ms)')
 legend(name,'Location','Southwest')
+
+subplot(2,1,1)
+title('Closed-Loop OFC')
---
+++ plant_comparison.m
@@ -0,0 +1,51 @@
+% Change the delay on line 10 to change how much delay you would like to
+% approximate.
+
+clear all;
+rng(1);
+
+% Single joint reaching movements:
+G = .14;        % Viscous Constant: Ns/m
+I = .1;         % Inertia Kgm2
+tau = 0.066;    % Muscle time constant, s
+delay = 0.250; 
+
+A1 = [0 1 0
+    0 -G/I 1/I
+    0 0 -1/tau];
+B1 = [0 0 1/tau]';
+C1 = [1 0 0];
+
+A2 = [0 1 0 0
+    0 -G/I 2/(delay*I) -1/I
+    0 0 -1/delay 1
+    0 0 0 1/tau];
+B2 = [0 0 0 1/tau]';
+C2 = [1 0 0 0];
+
+G = ss(A1,B1,C1,0);
+H = ss(A2,B2,C2,0);
+freq = 0.01:0.01:1000;
+[mag1, phase1] = bode(G,freq);
+[mag2, phase2] = bode(H,freq);
+mag1 = 20*log10(mag1);
+mag2 = 20*log10(mag2);
+phase1 = squeeze(phase1) + 90;
+phase2 = squeeze(phase2) - 90;
+
+lag = phase1 - phase2;
+lag = 1000*lag/360;
+
+figure
+subplot(2,1,1)
+semilogx(freq,phase1)
+hold on
+title('Open-Loop Plant')
+semilogx(freq,phase2)
+ylabel('Phase (deg)')
+legend({'Undelayed',['Delay = ',num2str(delay*1000),' ms']})
+
+subplot(2,1,2)
+semilogx(freq,lag)
+xlabel('Frequency (Hz)')
+ylabel('Time Difference (ms)')
